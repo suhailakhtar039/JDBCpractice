ca6f2f6ec5138c04753caa57b6bd4d009da34fb6
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-06-03 13:16:43 +0530

    sql query created

--- src/main/java/DemoJDBC.java
+++ src/main/java/DemoJDBC.java
@@ -14,10 +14,19 @@ public class DemoJDBC {
         String url = "jdbc:postgresql://localhost:5432/Student";
         String uname = "postgres";
         String pass = "suhail786";
+        String sql = "select sname from student where sid = 3";
 
         Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(url,uname, pass);
 
         System.out.println("Connection established");
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        System.out.println(rs.next());
+        conn.close();
+        System.out.println("Connection closed");
+
     }
 }
