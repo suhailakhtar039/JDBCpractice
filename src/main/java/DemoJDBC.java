ed0c1cb6e0dcb951f256077feef7a9c2ff13a5da
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-06-03 12:34:46 +0530

    connection established

--- src/main/java/DemoJDBC.java
+++ src/main/java/DemoJDBC.java
@@ -1,6 +1,23 @@
+import java.sql.*;
 
 public class DemoJDBC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        /*
+        import package
+        load and register
+        create connection
+        create statement
+        execute statement
+        process the results
+        close
+        */
+        String url = "jdbc:postgresql://localhost:5432/Student";
+        String uname = "postgres";
+        String pass = "suhail786";
 
+        Class.forName("org.postgresql.Driver");
+        Connection conn = DriverManager.getConnection(url,uname, pass);
+
+        System.out.println("Connection established");
     }
 }
